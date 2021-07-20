46483fffe996346c37f694d2418d2ffdf8064c0a
pixelstorm <dan@pixelstorm.com.au>
2021-07-20 21:00:48 +1000

    use simple calculation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 
 function App() {
   const name = "dan";
-  return <div className="App">Hello {name}</div>;
+  return (
+    <div className="App">
+      Hello {name} {1 + 1}
+    </div>
+  );
 }
 
 export default App;
