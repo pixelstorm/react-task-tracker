8c2f74f09e3f6a70fc7651aace05bf89d6fd0f81
pixelstorm <dan@pixelstorm.com.au>
2021-07-21 20:56:46 +1000

    use component and props for button for dynamic values

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+export const Button = ({ color, text }) => {
+  return (
+    <button style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
+import Button from "./Button";
 const Header = ({ title }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <button className="btn">Add</button>
+      <Button color="green" text="Hello" />
+      <Button color="pink" text="Hello moto" />
+      <Button color="salmon" text="Hello salmon" />
     </header>
   );
 };
