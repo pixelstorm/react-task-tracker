4c5dea9f8ede9ed78640cba076ea94282b222ad3
pixelstorm <dan@pixelstorm.com.au>
2021-07-24 17:42:27 +1000

    add the new task to app state

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App() {
 
   //addTask
   const addTask = (task) => {
-    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
   };
 
   //deleteTask
