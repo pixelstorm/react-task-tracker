68e16846a3197290ce44b615df4d117ffeac06a8
pixelstorm <dan@pixelstorm.com.au>
2021-07-24 13:07:57 +1000

    use filter in delete function to delete tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,13 @@
 import { FaTimes } from "react-icons/fa";
-export const Task = ({ task }) => {
+export const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
       <h3>
         {task.text}
-        <FaTimes style={{ color: "red", cursor: "pointer" }} />
+        <FaTimes
+          onClick={() => onDelete(task.id)}
+          style={{ color: "red", cursor: "pointer" }}
+        />
       </h3>
       <p>{task.day}</p>
     </div>
