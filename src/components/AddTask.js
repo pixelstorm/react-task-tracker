3536ba34c3f7e8d694337002465835b208e17315
pixelstorm <dan@pixelstorm.com.au>
2021-07-24 17:14:42 +1000

    set form field state to update onChange

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 export const AddTask = () => {
+  const [text, setText] = useState("default text from text State");
+  const [day, setDay] = useState("default day from day State");
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="add task" />
+        <input
+          type="text"
+          placeholder="add task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label>Day & Time</label>
-        <input type="text" placeholder="Add Day and time" />
+        <input
+          type="text"
+          placeholder="Add Day and time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
       <div className="form-control form-control-check">
         <label>Set Reminder</label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
       <input type="submit" value="save task" className="btn btn-block" />
     </form>
