98f2efde18ac159111b59200739e72db5c065ca9
pixelstorm <dan@pixelstorm.com.au>
2021-07-24 14:07:09 +1000

    fire function when task is doubleclicked

--- src/App.js
+++ src/App.js
@@ -29,11 +29,15 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleReminder = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks"
       )}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import { FaTimes } from "react-icons/fa";
-export const Task = ({ task, onDelete }) => {
+export const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div className="task" onDoubleClick={() => onToggle(task.id)}>
       <h3>
         {task.text}
         <FaTimes
