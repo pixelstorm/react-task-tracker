9a1f3004d1f5f6e66a1aa504946e6f31f2a56466
pixelstorm <dan@pixelstorm.com.au>
2021-07-24 15:23:49 +1000

    add form for creating new tasks

---
+++ src/components/AddTask.js
@@ -0,0 +1,20 @@
+export const AddTask = () => {
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="add task" />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input type="text" placeholder="Add Day and time" />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input type="checkbox" />
+      </div>
+      <input type="submit" value="save task" className="btn btn-block" />
+    </form>
+  );
+};
+export default AddTask;
