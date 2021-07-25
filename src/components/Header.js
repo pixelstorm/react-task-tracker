1ff9fdfb433f690c369c6a82b57470e97a5135bb
pixelstorm <dan@pixelstorm.com.au>
2021-07-25 10:56:29 +1000

    toggle btn text when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   const onClickFunc = () => {
     console.log("onClickFunc");
   };
@@ -9,7 +9,7 @@ const Header = ({ title, onAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onAdd} />
+      <Button color="green" text={showAdd ? "Close" : "Add"} onClick={onAdd} />
     </header>
   );
 };
